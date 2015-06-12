6855d72f0db1546ba574c8ae87bc2047fb989b77
Tony <CodeGuro@gmail.com>
2015-06-12 01:24:13 -0400

    fixed a bug with the nonlinear script that caused divergence

--- eval_sum_num_i.m
+++ eval_sum_num_i.m
@@ -3,14 +3,12 @@ function sum_num_i = eval_sum_num_i( i, vecX, matK, matN, powerSetsJ, alphas, al
     % begin evaluating the sum (numerator)
     for j = 1 : size( powerSetsJ{ i }, 2 )
         product = 1;
-        for it = 1 : size( powerSetsJ{ i }{ j }, 2 )
-            vec = powerSetsJ{ i }{ j };
-            alpha = alphas{ i }{ j };
-            for it2 = 1 : size( vec, 2 )
-                product = product * epsilonfunc( i, vec( it2 ), vecX, matK, matN );
-            end
-            product = product * alpha;
+        vec = powerSetsJ{ i }{ j };
+        alpha = alphas{ i }{ j };
+        for it2 = 1 : size( vec, 2 )
+            product = product * epsilonfunc( i, vec( it2 ), vecX, matK, matN );
         end
+        product = product * alpha;
         sum_num_i = sum_num_i + product;
     end
     sum_num_i = sum_num_i + alpha_null( i );
