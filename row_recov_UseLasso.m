78ef36603e9b8b293c84067005f1d6ef457d81c8
Tony <CodeGuro@gmail.com>
2015-07-07 21:39:53 -0400

    optimizations

--- row_recov_UseLasso.m
+++ row_recov_UseLasso.m
@@ -1,5 +1,4 @@
-function [rec_row, rec_row_lambads] = row_recov_UseLasso( matDeltaX_selected_row, matDeltaX_selected, lambdas )
+function rec_row_lambads = row_recov_UseLasso( matDeltaX_selected_row, matDeltaX_selected, lambdas )
     [ B, S ] = lasso( matDeltaX_selected', matDeltaX_selected_row', 'lambda', lambdas );
-    rec_row = B(:, 1)';
     rec_row_lambads = B';
 end
