1b7eee428b535047b7259be1d3e0048eead304e3
Tony <CodeGuro@gmail.com>
2015-08-10 18:12:50 -0400

    debugging proceeding (rowrecov)

--- matK_rec_useLasso.m
+++ matK_rec_useLasso.m
@@ -3,10 +3,11 @@ function matK_recs = matK_rec_useLasso( n, matdeltaX, lambdas )
     
     for p_idx = 1 : n
         selection = setdiff( 1:n, p_idx );
-         matK_cur_rows = row_recov_UseLasso( matdeltaX( p_idx, selection ), matdeltaX( selection, selection ), lambdas );
+        matK_cur_rows = row_recov_UseLasso( matdeltaX( p_idx, selection ), matdeltaX( selection, selection ), lambdas );
+        
+        rowrecov = row_recov_UseLasso( matdeltaX( p_idx, selection ), construct_perabolaMat( matdeltaX, n, p_idx ), lambdas );
         
         insertion_element = 0;
-        
         for z=1:size(matK_cur_rows,1)
             matK_recs( p_idx, :, z ) = insert(matK_cur_rows(z, :), insertion_element, p_idx);
         end
