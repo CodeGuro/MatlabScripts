7e145b8e61a0abb57f6d1f4ae8fbe755e6e999cf
Tony <CodeGuro@gmail.com>
2015-08-12 21:00:09 -0400

    feature commit, prototype

--- matK_rec_useLasso.m
+++ matK_rec_useLasso.m
@@ -14,14 +14,24 @@ function matK_recs = matK_rec_useLasso( n, matdeltaX, lambdas )
             disp(matN_cur_rows);
         end
         
-        Nmat = zeros( n, n );
-        
-        Nmat( nonzero_indices ) = matN_cur_rows( 1, :);
-        
         insertion_element = 0;
         for z=1:size(matK_cur_rows,1)
             matK_recs( p_idx, :, z ) = insert(matK_cur_rows(z, :), insertion_element, p_idx);
         end
+        
+        for lambda_idx = 1:length(lambdas)
+        	Nmat_gene_pidx = zeros( n, n );
+            Nmat_gene_pidx( nonzero_indices ) = matN_cur_rows( lambda_idx, : );
+            for i=1:n
+                for j=1:n
+                    if Nmat_gene_pidx( i, j ) ~= 0
+                        matK_recs( p_idx, i, lambda_idx ) = Inf;
+                        matK_recs( p_idx, j, lambda_idx ) = Inf;
+                    end
+                end
+            end
+        end
+        
     end
     
 end
