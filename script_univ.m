f2e08bd6efe6c928fa3cad27e1125cf5b8aa4977
Tony <CodeGuro@gmail.com>
2015-08-20 05:13:06 -0400

    experimental branching - plot vs n's

--- script_univ.m
+++ script_univ.m
@@ -1,16 +1,16 @@
 % options
 linear = false;
 use_lasso_Nmat = false;
-n = 5;
+nsizes = 3:10;
 A_limiter = 0.8;
-numMatrix_samples = 6;
+numMatrix_samples = 12;
 maxIterations = 1000;
 itDiff_threshold = 1E-4;
 perturb_amount = 1;
 num_samples = 20;
 mistake_threshold = 1E-1;
-sigmas = 0:0.03:0.5;
-lambdas = [2E-4 2E-2 0.1 0.2 1 2E10 ];
+sigmas = [ 0.01 ];
+lambdas = [2E-4 0.02 0.06 0.1 1 2E10 ];
 if linear
     func_type = 'linear';
 else
@@ -18,16 +18,22 @@ else
 end
 
 % algorithm starts here
-[ numMisakes_avg_Msamples, numMisakes_devs_Msamples ] = sample_numMistakes_nmatrices( n, ...
-    itDiff_threshold, mistake_threshold, maxIterations, numMatrix_samples, ...
-    perturb_amount, num_samples, lambdas, sigmas, linear, use_lasso_Nmat, ...
-    A_limiter );
+numMistakes_avg_nSizes = nan( length(nsizes), 1 + length(lambdas) );
+numMistakes_std_nSizes = nan( length(nsizes), 1 + length(lambdas) );
+for n_it = 1: length(nsizes)
+    n = nsizes(n_it);
+    [ numMisakes_avg_Msamples, numMisakes_devs_Msamples ] = sample_numMistakes_nmatrices( n, ...
+        itDiff_threshold, mistake_threshold, maxIterations, numMatrix_samples, ...
+        perturb_amount, num_samples, lambdas, sigmas, linear, use_lasso_Nmat, ...
+        A_limiter );
+    numMistakes_avg_nSizes( n_it, : ) = numMisakes_avg_Msamples;
+    numMistakes_std_nSizes( n_it, : ) = numMisakes_devs_Msamples;
+end
 
 % output
-
-plot_x = (ones( 1 + length(lambdas), 1 ) * sigmas)';
-plot_y = numMisakes_avg_Msamples;
-plot_devs = numMisakes_devs_Msamples;
+plot_x = (ones( 1 + length(lambdas), 1 ) * nsizes)';
+plot_y = numMistakes_avg_nSizes;
+plot_devs = numMistakes_std_nSizes;
 legends = cell( 1, 1 + length(lambdas) );
 
 legends{1} = 'inv';
@@ -37,7 +43,7 @@ end
 
 errorbar( plot_x, plot_y, plot_devs, ':o' );
 legend( 'avg mistakes' );
-xlabel('sigma');
+xlabel('n_size');
 ylabel('recovery mistakes');
 legend( legends );
 title( {[num2str(n) 'X' num2str(n) ' matrix of a ' func_type ' regulatory network, with '...
