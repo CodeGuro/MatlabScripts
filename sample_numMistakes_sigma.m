0965da77c426dd8c66204b52faf06b4ed12aebd1
Tony <CodeGuro@gmail.com>
2015-08-27 01:58:41 -0400

    cleanup

--- sample_numMistakes_sigma.m
+++ sample_numMistakes_sigma.m
@@ -15,18 +15,6 @@ function [ vecMistakes_avg, vecMistakes_dev  ] = sample_numMistakes_sigma( n, st
         matdeltaX( :, ((sample_num-1)*n+1): (sample_num*n) ) = construct_matdeltaX( n, steady_vecX, vecB, matK, matN, setsJ, powerSetsJ, ...
             alphas, alpha_null, itDiff_threshold, maxIterations, perturb_amount, sigma, linear );
 
-%         % We can now attempt to construct the linear matrix using the offsets
-%         matK_rec = matK_rec_useInv( n, matdeltaX );
-%         vecMistakes_inv( sample_num ) = nnz( logical( matK ) - logical( abs(matK_rec) > mistake_threshold ) );
-% 
-%         if length(lambdas) > 0
-%             matK_recs_lasso = matK_rec_useLasso( n, matdeltaX, lambdas, use_lasso_Nmat );
-%             for z=1:length(lambdas)
-%                 numMistakes_lasso = nnz( logical( matA ) - logical( abs( matK_recs_lasso(:,:,z) ) > mistake_threshold ) );
-%                 vecMistakes_lasso( sample_num, z ) = numMistakes_lasso;
-%             end
-%         end
-
     end
     
     matK_rec = matK_rec_useInv( n, matdeltaX, num_samples );
