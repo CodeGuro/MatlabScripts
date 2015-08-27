775ba72d109307c34fca22a7ed1760bf3fcc6b3f
Tony <CodeGuro@gmail.com>
2015-08-27 01:09:20 -0400

    numSamples functions differently
    this commit breaks Nmat feature

--- matK_rec_useInv.m
+++ matK_rec_useInv.m
@@ -1,10 +1,11 @@
-function matK_rec = matK_rec_useInv( n, matdeltaX )
+function matK_rec = matK_rec_useInv( n, matdeltaX, num_samples )
 
     matK_rec = nan( n, n );
     
     for p_idx = 1 : n
-        selection = setdiff( 1:n, p_idx );
-        matK_cur_row = row_recov_UseInv( matdeltaX( p_idx, selection ), matdeltaX( selection, selection ) );
+        selection_rows = setdiff( 1:n, p_idx );
+        selection_cols = setdiff( 1:n*num_samples, p_idx + (0:(num_samples-1))*n );
+        matK_cur_row = row_recov_UseInv( matdeltaX( p_idx, selection_cols ), matdeltaX( selection_rows, selection_cols ) );
         matK_cur_row = insert( matK_cur_row, 0, p_idx );
         matK_rec( p_idx, : ) = matK_cur_row;
     end
--- sample_numMistakes_sigma.m
+++ sample_numMistakes_sigma.m
@@ -2,28 +2,42 @@ function [ vecMistakes_avg, vecMistakes_dev  ] = sample_numMistakes_sigma( n, st
                 alphas, alpha_null, itDiff_threshold, mistake_threshold, maxIterations, perturb_amount, num_samples, ...
                 lambdas, sigma, linear, use_lasso_Nmat )
 
-    vecMistakes_inv = nan( 1, num_samples );
-    vecMistakes_lasso = nan( num_samples, length(lambdas) );
+    trials = 1; % num_samples (previously)
+    vecMistakes_inv = nan( 1, trials );
+    vecMistakes_lasso = nan( trials, length(lambdas) );
+    
+    matdeltaX = nan( n, n*num_samples );
 
     for sample_num = 1:num_samples
 
         disp(['iterating... sample ' num2str(sample_num) ' out of ' num2str(num_samples)]);
 
-        matdeltaX = construct_matdeltaX( n, steady_vecX, vecB, matK, matN, setsJ, powerSetsJ, ...
+        matdeltaX( :, ((sample_num-1)*n+1): (sample_num*n) ) = construct_matdeltaX( n, steady_vecX, vecB, matK, matN, setsJ, powerSetsJ, ...
             alphas, alpha_null, itDiff_threshold, maxIterations, perturb_amount, sigma, linear );
 
-        % We can now attempt to construct the linear matrix using the offsets
-        matK_rec = matK_rec_useInv( n, matdeltaX );
-        vecMistakes_inv( sample_num ) = nnz( logical( matK ) - logical( abs(matK_rec) > mistake_threshold ) );
+%         % We can now attempt to construct the linear matrix using the offsets
+%         matK_rec = matK_rec_useInv( n, matdeltaX );
+%         vecMistakes_inv( sample_num ) = nnz( logical( matK ) - logical( abs(matK_rec) > mistake_threshold ) );
+% 
+%         if length(lambdas) > 0
+%             matK_recs_lasso = matK_rec_useLasso( n, matdeltaX, lambdas, use_lasso_Nmat );
+%             for z=1:length(lambdas)
+%                 numMistakes_lasso = nnz( logical( matA ) - logical( abs( matK_recs_lasso(:,:,z) ) > mistake_threshold ) );
+%                 vecMistakes_lasso( sample_num, z ) = numMistakes_lasso;
+%             end
+%         end
 
-        if length(lambdas) > 0
-            matK_recs_lasso = matK_rec_useLasso( n, matdeltaX, lambdas, use_lasso_Nmat );
-            for z=1:length(lambdas)
-                numMistakes_lasso = nnz( logical( matA ) - logical( abs( matK_recs_lasso(:,:,z) ) > mistake_threshold ) );
-                vecMistakes_lasso( sample_num, z ) = numMistakes_lasso;
-            end
+    end
+    
+    matK_rec = matK_rec_useInv( n, matdeltaX, num_samples );
+    vecMistakes_inv( 1 ) = nnz( logical( matK ) - logical( abs(matK_rec) > mistake_threshold ) );
+    
+    if length(lambdas) > 0
+        matK_recs_lasso = matK_rec_useLasso( n, matdeltaX, num_samples, lambdas, use_lasso_Nmat );
+        for z=1:length(lambdas)
+            numMistakes_lasso = nnz( logical( matA ) - logical( abs( matK_recs_lasso(:,:,z) ) > mistake_threshold ) );
+            vecMistakes_lasso( 1, z ) = numMistakes_lasso;
         end
-
     end
     
     vecMistakes_avg = [ mean( vecMistakes_inv ) mean( vecMistakes_lasso, 1 ) ];
