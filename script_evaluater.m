b09ee78a0fdc0f6dda57fe86710264a776654279
Tony <CodeGuro@gmail.com>
2015-11-09 22:00:27 -0500

    logarithmic scaling for better viewing, bugfixes

--- script_evaluater.m
+++ script_evaluater.m
@@ -6,38 +6,9 @@ fname_reps = 'repetitions';
 mkdir( fname_pert );
 mkdir( fname_reps );
 
-yvec_lasso = nan( 1, length( numsamples_vec ) );
-yvec_inv = nan( 1, length( numsamples_vec ) );
-
-for j=1:length( numsamples_vec )
-    yvec_lasso( j ) = min( min( numMistakes_lasso_avg(:,j,:) ) );
-    yvec_inv( j ) = min( min( numMistakes_inv_avg(:,j,:) ) );
-end
-
-fig = plot( numsamples_vec, yvec_lasso );
-legend( 'mistakes across repetitions' );
-xlabel( 'samples' ); ylabel( 'mistakes' );
-title('min(lambdasXperturb), avgMat, lasso recovery smethod');
-saveas( fig, [ fname_reps '/lasso_avg' ] );
-
-fig = plot( numsamples_vec, yvec_inv );
-legend( 'mistakes across repetitions' );
-title('min(thresholdxperturb), avgMat, linear recovery method');
-xlabel( 'samples' ); ylabel( 'mistakes' );
-saveas( fig, [fname_reps '/inv_avg' ] );
-
 % save bulk
 for ms = 1:numMatSamplesExternal_int
-    for j=1:length( numsamples_vec )
-        yvec_lasso( j ) = min( min( numMistakes_lasso(:,j,:,ms) ) );
-        yvec_inv( j ) = min( min( numMistakes_inv(:,j,:,ms) ) );
-    end
-    
-    mat_path = [ fname_reps '/mat' num2str(ms) ];
-    mkdir( mat_path );
-    bisect_path = [ mat_path '/bisect' ];
-    mkdir( bisect_path );
-    
+   
     surface_path = [mat_path '/surface'];
     mkdir( surface_path );
     
@@ -54,6 +25,7 @@ for ms = 1:numMatSamplesExternal_int
         xlabel( 'perturbation amount' );
         ylabel( 'threshold' );
         zlabel( 'mistakes' );
+        set(gca, 'XScale', 'log', 'YScale', 'log' );
         title( { 'perturbation X threshold ' ,[ 'slice at repetitions = ' num2str(numsamples_vec( it )) ] } );
         saveas( fig, [lin_sbisect_path '/rep_' num2str(it)] );
     end
@@ -68,6 +40,7 @@ for ms = 1:numMatSamplesExternal_int
         xlabel( 'perturbation amount' );
         ylabel( 'lambda' );
         zlabel( 'mistakes' );
+        set(gca, 'XScale', 'log', 'YScale', 'log' );
         title( { 'perturbation X lambda ' ,[ 'slice at repetitions = ' num2str(numsamples_vec( it )) ] } );
         saveas( fig, [lasso_sbisect_path '/rep_' num2str(it)] );
     end
@@ -115,7 +88,9 @@ saveas( fig, [ fname_pert '/lin_avg' ] );
 
 % save bulk
 lasso_pert = [fname_pert '/lasso'];
+mkdir( lasso_pert );
 lin_pert = [fname_pert '/linear'];
+mkdir( lin_pert );
 for ms = 1:numMatSamplesExternal_int
     for i=1:length( perturbAmount_vec )
         yvec_lasso( i ) = min( min( numMistakes_lasso(i,:,:,ms) ) );
